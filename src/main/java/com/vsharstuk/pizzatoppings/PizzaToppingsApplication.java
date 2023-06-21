8708ca96e8d2fb98424d5f20cd6b4578497d453e
Vitali Sharstuk <vitaliysherstuk@gmail.com>
2023-06-21 14:15:24 +0300

    Initial commit

---
+++ src/main/java/com/vsharstuk/pizzatoppings/PizzaToppingsApplication.java
@@ -0,0 +1,13 @@
+package com.vsharstuk.pizzatoppings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaToppingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaToppingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsharstuk/pizzatoppings/PizzaToppingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsharstuk.pizzatoppings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaToppingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
