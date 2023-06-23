6c88743d6a619334e9107a34e6dfe5482f2371b6
Vitali Sharstuk <vitaliysherstuk@gmail.com>
2023-06-23 17:25:14 +0300

    [migration] : cleanup and commit

--- src/main/kotlin/com/vsharstuk/pizzatoppings/controller/ToppingReportController.kt
+++ src/main/kotlin/com/vsharstuk/pizzatoppings/controller/ToppingReportController.kt
@@ -1,26 +1,24 @@
-package com.vsharstuk.pizzatoppings.controller;
+package com.vsharstuk.pizzatoppings.controller
 
-import com.vsharstuk.pizzatoppings.dto.ToppingReportDto;
-import com.vsharstuk.pizzatoppings.service.ToppingService;
-import com.vsharstuk.pizzatoppings.util.PageableUtils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.vsharstuk.pizzatoppings.dto.ToppingReportDto
+import com.vsharstuk.pizzatoppings.service.ToppingService
+import com.vsharstuk.pizzatoppings.util.PageableUtils.of
+import lombok.RequiredArgsConstructor
+import org.springframework.data.domain.Page
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/report/toppings")
 @RequiredArgsConstructor
-public class ToppingReportController {
-
-    private final ToppingService toppingService;
-
+class ToppingReportController {
+    private val toppingService: ToppingService? = null
     @GetMapping
-    public Page<ToppingReportDto> generateReport (@RequestParam(name = "page_number", required = false, defaultValue = "1") Integer pageNumber,
-                                                  @RequestParam(name = "page_size", required = false, defaultValue = "2") Integer pageSize,
-                                                  @RequestParam(name = "sort", required = false) String sort) {
-        return toppingService.countUsersPerToppings(PageableUtils.of(pageNumber, pageSize, sort, "amount"));
+    fun generateReport(@RequestParam(name = "page_number", required = false, defaultValue = "1") pageNumber: Int?,
+                       @RequestParam(name = "page_size", required = false, defaultValue = "2") pageSize: Int?,
+                       @RequestParam(name = "sort", required = false) sort: String?): Page<ToppingReportDto?>? {
+        return toppingService!!.countUsersPerToppings(of(pageNumber!!, pageSize!!, sort, "amount"))
     }
 }
--- src/main/kotlin/com/vsharstuk/pizzatoppings/controller/UserToppingController.kt
+++ src/main/kotlin/com/vsharstuk/pizzatoppings/controller/UserToppingController.kt
@@ -1,54 +1,40 @@
-package com.vsharstuk.pizzatoppings.controller;
+package com.vsharstuk.pizzatoppings.controller
 
-import com.vsharstuk.pizzatoppings.dto.UserDto;
-import com.vsharstuk.pizzatoppings.dto.UserToppingRequest;
-import com.vsharstuk.pizzatoppings.entity.Topping;
-import com.vsharstuk.pizzatoppings.entity.User;
-import com.vsharstuk.pizzatoppings.mapper.ToppingMapper;
-import com.vsharstuk.pizzatoppings.mapper.UserMapper;
-import com.vsharstuk.pizzatoppings.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.vsharstuk.pizzatoppings.dto.ToppingDto
+import com.vsharstuk.pizzatoppings.dto.UserDto
+import com.vsharstuk.pizzatoppings.dto.UserToppingRequest
+import com.vsharstuk.pizzatoppings.mapper.ToppingMapper
+import com.vsharstuk.pizzatoppings.mapper.UserMapper
+import com.vsharstuk.pizzatoppings.service.UserService
+import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import java.util.stream.Collectors
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-public class UserToppingController {
-
-    private final UserService userService;
-    private ToppingMapper toppingMapper;
-
-    private UserMapper userMapper;
-
+class UserToppingController {
+    private val userService: UserService? = null
+    private val toppingMapper: ToppingMapper? = null
+    private val userMapper: UserMapper? = null
     @PostMapping("/{user_id}/toppings")
     @ResponseStatus(HttpStatus.CREATED)
-    public void submitToppings(@PathVariable("user_id") Long userIid,
-                               @RequestBody @Valid UserToppingRequest request) {
-
-        List<Topping> toppings = request.getToppings().stream().map(toppingMapper::toToppingEntity).collect(Collectors.toList());
-        userService.submit(userIid, toppings);
+    fun submitToppings(@PathVariable("user_id") userIid: Long?,
+                       @RequestBody request: @Valid UserToppingRequest?) {
+        val toppings = request!!.toppings!!.stream().map { dto: ToppingDto? -> toppingMapper!!.toToppingEntity(dto) }.collect(Collectors.toList())
+        userService!!.submit(userIid!!, toppings)
     }
 
     @GetMapping("/{user_id}")
-    public UserDto getUser(@PathVariable("user_id") Long userId) {
-        return userMapper.toUserDto(userService.getUser(userId));
+    fun getUser(@PathVariable("user_id") userId: Long?): UserDto? {
+        return userMapper!!.toUserDto(userService!!.getUser(userId!!))
     }
 
     @PostMapping
-    public UserDto create(@RequestBody @Valid UserDto userDto) {
-        User user = userService.create(userMapper.toUserEntity(userDto));
-        return userMapper.toUserDto(user);
+    fun create(@RequestBody userDto: @Valid UserDto?): UserDto? {
+        val user = userService!!.create(userMapper!!.toUserEntity(userDto)!!)
+        return userMapper.toUserDto(user)
     }
 }
